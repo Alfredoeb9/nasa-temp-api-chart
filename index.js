9e83c500b6f1fefdd0b20cc6c032f8dfd61394f3
Alfredoeb9 <alfredoeb96@gmail.com>
2019-10-15 15:25:35 -0700

    displayed nasa global temp with graph

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-var ctx = document.getElementById('chart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+const xLabels = []
+const ytemps = []
 
-async function getData() {
-  const response = await fetch('test.csv')
-  const data = await response.text()
-  console.log(data)
+async function chartIt() {
+  await getData();
 
-  console.log(response);
-  
+  const ctx = document.getElementById('chart').getContext('2d');
+  const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: xLabels,
+          datasets: [{
+              label: 'Global Average Temperature',
+              data: ytemps,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      }
+  });
 }
 
-getData()
+chartIt();
 
+
+
+async function getData() {
+  const response = await fetch('ZonAnn.Ts+dSST.csv')
+  const data = await response.text()
+
+  // make a table and seperate from year and temp
+  const table = data.split('\n').slice(1);
+  table.forEach(row => {
+    const columns = row.split(',')
+    const year = columns[0]
+    xLabels.push(year)
+    const temp = columns[1]
+    ytemps.push(temp);
+    console.log(year, temp)
+  })
+  
+  
+}
